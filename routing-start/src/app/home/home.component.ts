d1b01724d083724e6a2620a23cb3c5734f00a8cf
Daniels <daniel.sadowski75@gmail.com>
2019-07-15 23:59:05 +0200

    added query parameters and fragment to the navigate button

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -14,6 +14,6 @@ export class HomeComponent implements OnInit {
   }
 
   onLoadServer(id: number) {
-    this.router.navigate(['servers'])
+    this.router.navigate(['servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'})
   }
 }
