fd6b658a1de34eba1b6c5292eb834fa4c11fe5b1
Daniels <daniel.sadowski75@gmail.com>
2019-07-15 22:52:18 +0200

    added routes with :id path

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activeRout: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.activeRout.snapshot.params['id'],
+      name: this.activeRout.snapshot.params['name']
+    }
   }
 
 }
