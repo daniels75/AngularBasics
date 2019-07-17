d818fef044ed303f556c1ab9e176a21ed1fe9da7
Daniels <daniel.sadowski75@gmail.com>
2019-07-18 00:39:24 +0200

    Added server resolver for routes

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from "@angular/router";
+import {ActivatedRoute, Data, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -18,6 +18,12 @@ export class ServerComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    )
+    /*
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
@@ -25,6 +31,7 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id'])
       }
     )
+     */
 
   }
   editServer() {
