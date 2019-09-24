38eb354d7bfdf6c0424eaa4a16e134bdc0d2e117
Daniels <daniel.sadowski75@gmail.com>
2019-09-24 19:34:04 +0200

    Added example of subscriber of interval

--- observable01/src/app/home/home.component.ts
+++ observable01/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private subscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    this.subscription = interval(1000).subscribe((value) => {
+      console.log(value)
+    });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
 
 }
