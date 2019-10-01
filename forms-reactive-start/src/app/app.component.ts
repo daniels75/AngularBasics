5a36957579fce84f329e8e0e58f560124e26a4d3
Daniels <daniel.sadowski75@gmail.com>
2019-10-01 23:08:49 +0200

    Added custom validator

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsers = ['Kris' , 'Anna'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData' : new FormGroup( {
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbidenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -32,5 +33,12 @@ export class AppComponent implements OnInit{
 
   getHobbies() {
       return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  forbidenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsers && this.forbiddenUsers.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
