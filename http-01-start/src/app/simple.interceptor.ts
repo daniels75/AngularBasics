d12e838a99bb55c9846e2a66c4c7a59d5ac85a49
Daniels <daniel.sadowski75@gmail.com>
2019-10-09 21:05:58 +0200

    Added response interceptor

--- http-01-start/src/app/simple.interceptor.ts
+++ http-01-start/src/app/simple.interceptor.ts
@@ -1,11 +1,18 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 
 export class SimpleInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Request has been intercepted by SimpleInterceptor')
 
-    return next.handle(req);
+    return next.handle(req).pipe(tap(event => {
+      console.log(event)
+      if (event.type == HttpEventType.Response) {
+        console.log('response body retrieved');
+        console.log(event.body);
+      }
+    }));
   }
 }
