43ecd83071e4b8f4615edc6b85a811121b231ee0
Daniels <daniel.sadowski75@gmail.com>
2019-10-17 21:30:46 +0200

    attached reducer actions to the component

--- ngrx-first-reducer/src/app/shopping-list/shopping-list.component.ts
+++ ngrx-first-reducer/src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,7 @@ import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from '../logging.service';
 import {Store} from "@ngrx/store";
 import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shoping-list.action';
 
 @Component({
   selector: 'app-shopping-list',
@@ -43,7 +44,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 
   ngOnDestroy() {
