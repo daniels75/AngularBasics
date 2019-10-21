b8ac885099415640f1b7967c35c2cd76aa553acf
Daniels <daniel.sadowski75@gmail.com>
2019-10-21 22:02:53 +0200

    Added app reducer store

--- ngrx-first-reducer/src/app/app.module.ts
+++ ngrx-first-reducer/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import {StoreModule} from "@ngrx/store";
 import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
+import {authReducer} from "./auth/store/auth.reducer";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -18,7 +19,10 @@ import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot({
+      shoppingList: shoppingListReducer,
+      auth: authReducer
+    })
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
